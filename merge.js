ecf49964783a72c252c79c32cb104cc3cd74194f
Rajayne <ryann.j.nealey@gmail.com>
2023-06-02 11:43:23 -0700

    Create merge function and pass test

--- merge.js
+++ merge.js
@@ -1,5 +1,31 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
 
 function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
+
+var arr1 = [1, 3, 4, 5];
+var arr2 = [2, 4, 6, 8];
+merge(arr1, arr2);
